9fbd27c8be78f9b268e9e94f655238a6f2c04f95
xKxAxKx <pekepooooong@gmail.com>
2016-11-12 18:49:47 +0900

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Blog Posts</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Posts</h1>
+    <ul>
+      <li><a href="">title</a></li>
+      <li><a href="">title</a></li>
+      <li><a href="">title</a></li>
+    </ul>
+  </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//
+// Route::get('/', function () {
+//     return 'hello!!!';
+// });
+//
+// Route::get('/{name}', function ($name) {
+//     return 'hello!!!' . $name;
+// });
+
+Route::get('/', function () {
+    return view('posts.index');
+});
